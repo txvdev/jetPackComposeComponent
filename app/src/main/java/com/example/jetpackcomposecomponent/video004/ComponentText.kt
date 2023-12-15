c8ff5728284e2218dbd4c28d222494926ad83ed3
txv.dev <txv.dev@gmail.com>
2023-12-15 10:53:32 +0100

    Carpeta 04-Component Text todos los ejemplos de componentText

--- app/src/main/java/com/example/jetpackcomposecomponent/video004/ComponentText.kt
+++ app/src/main/java/com/example/jetpackcomposecomponent/video004/ComponentText.kt
@@ -1,9 +1,21 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.jetpackcomposecomponent.video004
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -11,9 +23,11 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecomponent.ui.theme.JetPackComposeComponentTheme
 
+//Component Text
 @Composable
 fun MyText() {
     Column(Modifier.fillMaxSize()) {
@@ -37,7 +51,7 @@ fun MyText() {
                 )
             )
         )
-        Text(text="Esto es un ejemplo", fontSize = 30.sp)
+        Text(text = "Esto es un ejemplo", fontSize = 30.sp)
     }
 }
 
@@ -47,4 +61,53 @@ fun PreviewText() {
     JetPackComposeComponentTheme {
         MyText()
     }
+}
+
+//Component TextField
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTextField() {
+    var myText by remember { mutableStateOf("") }
+    TextField(value = myText, onValueChange = { myText = it })
+}
+
+@Composable
+fun MyTextFieldAdvance() {
+    var myText by remember { mutableStateOf("") }
+
+    TextField(
+        value = "",
+        onValueChange = {
+            myText =
+                if (it.contains("a")) {
+                    it.replace("a", "")
+                } else {
+                    it
+                }
+        },
+        label = { Text(text = "Introduce tu nombre") })
+}
+
+// TextFieldOutlinend
+
+@Composable
+fun MyTextFieldOutLined() {
+    var myText by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = myText,
+        onValueChange = { myText = it },
+        modifier = Modifier.padding(24.dp),
+        label = {
+            Text(
+                text = "Texto"
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Magenta,
+            unfocusedBorderColor = Color.Blue
+        )
+
+    )
 }
